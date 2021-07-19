ea988926715e9a94bf3394db42622fde5c0c2cc3
Valentin Schmitt <schmitt.h.valentin@gmail.com>
2021-07-19 11:58:27 +0200

    assessBA v1

--- src/assessBA.py
+++ src/assessBA.py
@@ -5,7 +5,6 @@ import os, sys, argparse, time
 from pprint import pprint
 from glob import glob
 import numpy as np
-#from scipy.spatial.transform import Rotation as R
 
 # PyValLib packages
 from PVL.PVL_Logger import SetupLogger, ProcessStdout
@@ -71,7 +70,7 @@ def ObjTsai(pathIn):
             dic[words[0]]=[float(part) for part in words[1].split()]
     return dic
 
-def Table_Line(lstWords, iLen=5, wordLen=15):
+def Table_Line(lstWords, iLen=5, wordLen=20):
     strOut= lstWords[0].ljust(iLen)+' | '
     strOut+=' | '.join([word.ljust(wordLen) for word in lstWords[1:]])
     print(strOut)
@@ -81,16 +80,26 @@ def Table_3DCentre(obj1, obj2):
     pt2=np.array(obj2['C'])
     diff=sum(np.power(pt1-pt2, 2))**0.5
     return str(round(diff, 3))
-def Table_Omega(obj1, obj2):
+def Table_Direction(obj1, obj2):
+    pt1=np.array(obj1['C'])
+    pt2=np.array(obj2['C'])
+    diff=(pt1-pt2)>0
+    lstStr=[]
+    for i in diff:
+        if i:
+            lstStr.append('+')
+        else:
+            lstStr.append('-')
+    return ','.join(lstStr)
+def Table_Angles(obj1, obj2):
+    from scipy.spatial.transform import Rotation as R
     pt1=np.array(obj1['R']).reshape(3,3)
     pt2=np.array(obj2['R']).reshape(3,3)
     rot1=R.from_matrix(pt1)
     rot2=R.from_matrix(pt2)
-
-    print(rot1.as_euler('zxy', degrees=True))
-    sys.exit()
-    diff=sum(np.power(pt1-pt2, 2))**0.5
-    return str(round(diff, 3))
+    diff=rot1.as_euler('zxy', degrees=True)-rot2.as_euler('zxy', degrees=True)
+    lstStr=tuple(np.round(diff,3).astype(str))
+    return ','.join(lstStr)
 def Table_2DPP(obj1, obj2):
     pt1=np.array(obj1['cu']+obj1['cv'])
     pt2=np.array(obj2['cu']+obj2['cv'])
@@ -98,6 +107,35 @@ def Table_2DPP(obj1, obj2):
     return str(round(diff, 2))
 def Table_Focal(obj1, obj2):
     return str(round(obj1['fu'][0]-obj2['fu'][0], 3))
+
+def set_axes_equal(ax):
+    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc..  This is one possible solution to Matplotlib's
+    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+
+    Input
+      ax: a matplotlib axis, e.g., as output from plt.gca().
+    '''
+
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])
+    x_middle = np.mean(x_limits)
+    y_range = abs(y_limits[1] - y_limits[0])
+    y_middle = np.mean(y_limits)
+    z_range = abs(z_limits[1] - z_limits[0])
+    z_middle = np.mean(z_limits)
+
+    # The plot bounding box is a sphere in the sense of the infinity
+    # norm, hence I call half the max range the plot radius.
+    plot_radius = 0.5*max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+
 #=======================================================================
 #main
 #-----------------------------------------------------------------------
@@ -113,9 +151,13 @@ if __name__ == "__main__":
         parser.add_argument('-ba',required=True ,nargs='+' ,help='path to next BA folder in the right order')
         
         #Optional arguments
-        parser.add_argument('-noGraph',action='store_false',help='do not return the graph')
-        parser.add_argument('-noTable',action='store_false',help='do not return the table')
-        
+        parser.add_argument('-noGraph',action='store_false',help='Do not return the graph (intactive graph with matplotlib)')
+        parser.add_argument('-noTable',action='store_false',help='Do not return the table')
+        parser.add_argument('-longTable',action='store_true',help='Long table version (make use of scipy 1.7)')
+        parser.add_argument('-s', type=int, default=100, help='Graph: scale factor')
+        parser.add_argument('-ori',action='store_true',help='Graph: orienation arrow')
+        parser.add_argument('-e', action='store_false', help='Graph: do not display the Earth')
+
         args = parser.parse_args()
         
         #---------------------------------------------------------------
@@ -158,7 +200,7 @@ if __name__ == "__main__":
         if args.noTable:
             logger.info('# Summary table')
             Table_Line(['ImgID','Key', 'Init']+lstBaName)
-            for kind in ('3DCentre', 'Omega', 'Focal', '2DPP'):
+            for kind in ('3DCentre', 'Direction', 'Angles', 'Focal', '2DPP'):
                 print(kind+':')
                 i=-1
                 for key in dicTsai:
@@ -166,12 +208,15 @@ if __name__ == "__main__":
                     
                     i+=1
                     lstCur=dicTsai[key]
-                    
+                    Table_Direction(lstCur[2], lstCur[0])
                     if kind=='3DCentre':
                         lstOut=[Table_3DCentre(obj, lstCur[0]) for obj in lstCur]
-                    elif kind=='Omega':
-                        pass
-                        #lstOut=[Table_Omega(obj, lstCur[0]) for obj in lstCur]
+                    elif kind=='Direction':
+                        if not args.longTable: continue
+                        lstOut=[Table_Direction(obj, lstCur[0]) for obj in lstCur]
+                    elif kind=='Angles':
+                        if not args.longTable: continue
+                        lstOut=[Table_Angles(obj, lstCur[0]) for obj in lstCur]
                     elif kind=='Focal':
                         lstOut=[Table_Focal(obj, lstCur[0]) for obj in lstCur]
                     elif kind=='2DPP':
@@ -180,6 +225,7 @@ if __name__ == "__main__":
 
 
                     Table_Line([str(i),'Diff '+kind]+lstOut)
+
         
         #---------------------------------------------------------------
         # Graph
@@ -189,9 +235,11 @@ if __name__ == "__main__":
             import matplotlib.pyplot as plt
             from mpl_toolkits.mplot3d import Axes3D
             
-            #fig = plt.figure(1,[15,9])
-            #graph=fig.add_subplot(111, projection='3d')
+            fig = plt.figure(1,[15,9])
+            graph=fig.add_subplot(111, projection='3d')
             
+            
+
             i=-1
             for key in dicTsai:
                 if len(dicTsai[key])==1: continue
@@ -199,28 +247,56 @@ if __name__ == "__main__":
                 i+=1
                 lstCur=dicTsai[key]
                 
-                # Camera journey
                 matPts=np.array([obj['C'] for obj in lstCur])
-                graph.plot(matPts[:,0], matPts[:,1], matPts[:,2],'k-')
+                matSclPts=np.vstack((matPts[[0],:],
+                                    (matPts[1:,:]-matPts[0,:])*args.s+matPts[0,:]
+                                    ))
 
-                print(matPts)
-                sys.exit()
-                # Camera points
-                graph.plot(matPts[0,0], matPts[0,1], matPts[0,2],'g^',label='')
-                graph.text(matPts[0,0]+1, matPts[0,1]+1, matPts[0,2]+1,str(i))
-
-                if j in range(1,matPts.shape[0]):
-                    graph.plot(matPts[0,0], matPts[0,1], matPts[0,2],'bo',label='')
+                # Camera journey
+                graph.plot(matSclPts[:,0], matSclPts[:,1], matSclPts[:,2],'k--')
                 
+                vect=np.array([[0],[0],[1]])*args.s
+                for j in range(1,matSclPts.shape[0]):
+                    # Camera centre BA
+                    graph.plot(matSclPts[j,0], matSclPts[j,1], matSclPts[j,2],'o',label='')
+                    # Camera orientation BA 
+                    matR=np.array(lstCur[j]['R']).reshape(3,3)
+                    vectR=matR@vect
+                    if args.ori: graph.quiver(matSclPts[j,0], matSclPts[j,1], matSclPts[j,2],
+                                              vectR[0], vectR[1], vectR[2],
+                                              length=50,color='k')
+                
+                # Camera centre Init
+                graph.plot(matPts[0,0], matPts[0,1], matPts[0,2],'r^',label='', markersize=10)
+                graph.text(matPts[0,0]+args.s, matPts[0,1]+args.s, matPts[0,2]+args.s,str(i))
+
+                # Camera orientation Init
+                matR=np.array(lstCur[0]['R']).reshape(3,3)
+                vectR=matR@vect
+                if args.ori: graph.quiver(matSclPts[0,0], matSclPts[0,1], matSclPts[0,2],
+                                          vectR[0], vectR[1], vectR[2],
+                                          length=50, color='r')
+                
+            #Earth shape
+            set_axes_equal(graph)
+
+            if args.e:
+                rEarth=6.371e6
+                u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+                x = np.cos(u)*np.sin(v)*rEarth
+                y = np.sin(u)*np.sin(v)*rEarth
+                z = np.cos(v)*rEarth
+                #graph.plot_surface(x, y, z, color='b')
+                graph.plot_wireframe(x, y, z, color="b")
 
 
-            #graph.set_xlabel('X')
-            #graph.set_ylabel('Y')
-            #graph.set_ylabel('Z')
+            graph.set_xlabel('X (ECEF)')
+            graph.set_ylabel('Y (ECEF)')
+            graph.set_zlabel('Z (ECEF)')
             #graph.legend()
-            #
-            #fig.suptitle('Camera centres')
-            #plt.show()
+            
+            fig.suptitle('Camera centres (scale %i)'% args.s)
+            plt.show()
 
 
         
