4450b3ec2909ac2f55f300269cb99f50f3f2215f
Valentin Schmitt <schmitt.h.valentin@gmail.com>
2021-08-27 15:58:07 +0200

    AssessBA V2.4

--- src/AssessBaFunc.py
+++ src/AssessBaFunc.py
@@ -88,11 +88,10 @@ class ReadBA:
                 with open(pathResInit) as fileIn:
                     checkCamPart=False
                     for lineCur in fileIn:
-                        lineClean=lineCur.strip()
-                        if lineClean.startswith('Camera'): checkCamPart=True
-                        if not lineClean.startswith('/'): continue
+                        if lineCur.startswith('Camera'): checkCamPart=True
+                        if not lineCur.startswith('/'): continue
                         
-                        words=lineClean.split(', ')
+                        words=lineCur.strip().split(', ')
                         name=os.path.basename(words[0]).strip(self.prefix)
                         
                         if not checkCamPart:
@@ -101,7 +100,7 @@ class ReadBA:
                         else:
                             self.dicTsai[name][-1]['resiTrans-%s'% step]=float(words[1])
                             self.dicTsai[name][-1]['resiRot-%s'% step]=float(words[2])
-        
+    
     def Valid(self):
         if len(self.dicTsai)==1: logger.warning('Only one scene in the assessment')
         
@@ -114,9 +113,44 @@ class ReadBA:
             lstCheck+=['numKP-%s'% step in self.dicTsai[key][i] for key in self.dicTsai for i in range(1,len(self.dicTsai[key]))]
         if not all(lstCheck): msg='Residuals (initial and/or final) not found'
         return msg
-        
 
-def Table(dicTsai, nbBa, longT=True, intrinsic=True):
+def KP2Matrix(pathDir):
+    '''
+    Read initial and final point coordinate and store them into a matrix
+    '''
+
+    # Long0, Lat0, Height0, Res0, Long1, Lat1, Height1, Res1, GcpBool
+    matIn=np.empty([9])
+
+    # Read KP
+    for i,step in enumerate(('initial', 'final')):
+        pathKpCsv=glob(os.path.join(pathDir,'*%s_residuals_no_loss_function_pointmap_point_log.csv'% step))
+        if not pathKpCsv: continue
+
+        pathKpCsv=pathKpCsv[0]
+        with open(pathKpCsv) as fileIn:
+            txtIn=[lineCur for lineCur in fileIn.readlines() if not lineCur.startswith('#')]
+        if not i :
+            # Long0, Lat0, Height0, Res0, Long1, Lat1, Height1, Res1, GcpBool
+            matIn=np.ones([len(txtIn),9])*np.nan
+
+        for j,lineCur in enumerate(txtIn):
+            if lineCur.startswith('#'): continue
+            
+            words=lineCur.strip().split(', ')
+
+            matIn[j,i*4:i*4+4]=[float(k) for k in words[:4]]
+            if i and words[-1].endswith('GCP'):
+                matIn[j,-1]=1
+            else:
+                matIn[j,-1]=0
+
+    # Long0, Lat0, Height0, Res0, DLong, DLat1, DHeight, Res1, GcpBool
+    matOut=np.hstack((matIn[:,:4],matIn[:,4:8]-matIn[:,0:4], matIn[:, [-1]]))
+
+    return matOut
+
+def Table(dicTsai, nbBa, argsCur):
     '''
     Table function
     '''
@@ -124,14 +158,14 @@ def Table(dicTsai, nbBa, longT=True, intrinsic=True):
     
     # Difference table
     print('='*(colLen[0]+10)+'{:^26}'.format('Differences Table')+'='*(13+colLen[1]*nbBa+3*(nbBa-1)))
-    Table_Line('ImgID','Key', ['Init']+lstBaName)
+    Table_Line('ImgID','Name', ['Init']+lstBaName)
     lstKind=['3DCentre[m]', 'Angles[°]']
-    if intrinsic: lstKind+=['Focal[mm]', '2DPP[mm]']
+    if argsCur.io: lstKind+=['Focal[mm]', '2DPP[mm]']
     for kind in lstKind:
-        print('\n'+kind+':')
+        print(kind+':')
 
         mat=np.ones(shapeCur)*np.nan
-        for i, sceneId in enumerate(dicTsai):                    
+        for i, sceneId in enumerate(sorted(dicTsai)):                    
             lstCur=dicTsai[sceneId]
             if not len(lstCur)==1+nbBa: continue
 
@@ -148,23 +182,20 @@ def Table(dicTsai, nbBa, longT=True, intrinsic=True):
                 mat[i,:]=[np.round(norm(obj['matPP']-lstCur[0]['matPP']), 3)
                                 for obj in lstCur]
 
-
-
-
-            if longT: Table_Line(i, 'Diff '+kind, list(mat[i,:]))
+            if argsCur.longTable: Table_Line(i, 'Diff '+kind, list(mat[i,:]))
         
-        Table_Sum(mat, kind)
+        Table_Sum(mat, 'Diff '+kind)
     print('.'*(colLen[0]+colLen[1]*(2+nbBa)+9+3*(nbBa-1)))
 
     # Residual table
     print('='*(colLen[0]+10)+'{:^26}'.format('Residuals Table')+'='*(13+colLen[1]*nbBa+3*(nbBa-1)))
-    Table_Line('ImgID', 'Key', ['Init']+lstBaName)
+    Table_Line('ImgID', 'Name', ['Init']+lstBaName)
     
     for kind in ('Tranlation[m]', 'Rotation[°]', 'KeyPoints[pxl]'):
-        print('\n'+kind+':')
+        print(kind+':')
 
         mat=np.ones(shapeCur)*np.nan
-        for i, sceneId in enumerate(dicTsai):
+        for i, sceneId in enumerate(sorted(dicTsai)):
             lstCur=dicTsai[sceneId]
             
             if kind=='Tranlation[m]':
@@ -174,9 +205,9 @@ def Table(dicTsai, nbBa, longT=True, intrinsic=True):
             elif kind=='KeyPoints[pxl]':
                 mat[i,:]=[0]+[round(obj['resiKP-final'], 1) for obj in lstCur[1:]]
             
-            if longT: Table_Line(i,'Resid '+kind, list(mat[i,:]))
+            if argsCur.longTable: Table_Line(i,'Resid '+kind, list(mat[i,:]))
         
-        Table_Sum(mat, kind)
+        Table_Sum(mat, 'Res '+kind)
     print('='*(colLen[0]+colLen[1]*(2+nbBa)+9+3*(nbBa-1)))
 
 def Table_Line(iCur, kindCur, lstVal, lenCur=colLen):
@@ -188,19 +219,19 @@ def Table_Line(iCur, kindCur, lstVal, lenCur=colLen):
 def Table_Sum(matIn, kindCur, lenCur=colLen):
     Table_Line('-'*lenCur[0], '-'*lenCur[1], ['-'*lenCur[1]]*(matIn.shape[1]))
     ave=np.round(np.nanmean(matIn, axis=0), 3)
-    Table_Line('Mean', 'Diff '+kindCur, ave)
+    Table_Line('Mean', kindCur, ave)
     rms=np.round(np.sqrt(np.nanmean(np.square(matIn), axis=0)), 3)
-    Table_Line('RMS', 'Diff '+kindCur, rms)
+    Table_Line('RMS', kindCur, rms)
     std=np.round(np.nanstd(matIn, axis=0), 3)
-    Table_Line('Std', 'Diff '+kindCur, std)
+    Table_Line('Std', kindCur, std)
     maxCur=np.round(np.nanmax(matIn, axis=0), 3)
-    Table_Line('Max', 'Diff '+kindCur, maxCur)
+    Table_Line('Max', kindCur, maxCur)
     minCur=np.round(np.nanmin(matIn, axis=0), 3)
-    Table_Line('Min', 'Diff '+kindCur, minCur)
+    Table_Line('Min', kindCur, minCur)
 
-def Graph(dicTsai, argsCur):
+def GraphEO(dicTsai, argsCur):
     '''
-    Graph function
+    3D Graph function with camera centre and orientation.
     '''
     import matplotlib.pyplot as plt
     from mpl_toolkits.mplot3d import Axes3D
@@ -223,6 +254,17 @@ def Graph(dicTsai, argsCur):
                             (matPts[1:,:]-matPts[0,:])*argsCur.s+matPts[0,:]
                             ))
 
+
+        # Camera centre Init
+        graph.plot(matPts[0,0], matPts[0,1], matPts[0,2],'r^',label='', markersize=10)
+        graph.text(matPts[0,0]+args.s, matPts[0,1]+args.s, matPts[0,2]+args.s,str(i))
+
+        # Camera orientation Init
+        vectR=lstCur[0]['matR']@vect
+        if argsCur.ori: graph.quiver(matSclPts[0,0], matSclPts[0,1], matSclPts[0,2],
+                                  vectR[0], vectR[1], vectR[2],
+                                  length=50, color='r')
+
         # Camera journey
         graph.plot(matSclPts[:,0], matSclPts[:,1], matSclPts[:,2],'k--')
         
@@ -239,16 +281,6 @@ def Graph(dicTsai, argsCur):
             if argsCur.ori: graph.quiver(matSclPts[j,0], matSclPts[j,1], matSclPts[j,2],
                                       vectR[0], vectR[1], vectR[2],
                                       length=50,color='k')
-        
-        # Camera centre Init
-        graph.plot(matPts[0,0], matPts[0,1], matPts[0,2],'r^',label='', markersize=10)
-        graph.text(matPts[0,0]+args.s, matPts[0,1]+args.s, matPts[0,2]+args.s,str(i))
-
-        # Camera orientation Init
-        vectR=lstCur[0]['matR']@vect
-        if argsCur.ori: graph.quiver(matSclPts[0,0], matSclPts[0,1], matSclPts[0,2],
-                                  vectR[0], vectR[1], vectR[2],
-                                  length=50, color='r')
         
     #Earth shape
     set_axes_equal(graph)
@@ -269,6 +301,32 @@ def Graph(dicTsai, argsCur):
     graph.legend()
     
     fig.suptitle('Camera centres (scale %i)'% argsCur.s)
+    plt.show()
+
+def GraphKP(lstIn, argsCur):
+    '''
+    Graph function with KP
+    '''
+    import matplotlib.pyplot as plt
+    import matplotlib.colors as mcolors
+
+    nbBA=len(lstIn)
+    fig, graph = plt.subplots()
+    lstColours=list(mcolors.TABLEAU_COLORS.values())
+
+    for i in range(nbBA):
+        matCur=lstIn[i]
+
+        graph.quiver(matCur[:,0], matCur[:,1], # matSclPts[j,2],
+                    matCur[:,4], matCur[:,5], #vectR[2],
+                    color=lstColours[i],
+                    label=lstBaName[i])
+        
+    graph.set_xlabel('Long')
+    graph.set_ylabel('Lat')
+    graph.legend()
+    graph.axis('equal')
+    fig.suptitle('Key point displacement')
     plt.show()
 
 def set_axes_equal(ax):
@@ -319,12 +377,12 @@ if __name__ == "__main__":
         parser.add_argument('-longTable',action='store_true',help='Long table version')
         parser.add_argument('-io', action='store_true', help='Tabe: print intrinsic parameter difference')
 
-        parser.add_argument('-graph',action='store_true',help='Return the graph (interactive graph with matplotlib)')
+        parser.add_argument('-graphEO',action='store_true',help='Return the graph (interactive graph with matplotlib)')
         parser.add_argument('-s', type=int, default=100, help='Graph: scale factor')
         parser.add_argument('-ori',action='store_true',help='Graph: orienation arrow')
         parser.add_argument('-e', action='store_true', help='Graph: display the Earth')
 
-        parser.add_argument('-kp',action='store_true',help='Return the KP sketch (interactive graph with matplotlib)')
+        parser.add_argument('-graphKP',action='store_true',help='Return the KP sketch (interactive graph with matplotlib)')
 
         args = parser.parse_args()
         
@@ -345,27 +403,38 @@ if __name__ == "__main__":
         #sys.exit()
         print()
 
-        #---------------------------------------------------------------
-        # Read initial step
-        #---------------------------------------------------------------
-        logger.info('# Read initial step')
-        logger.info('Prefix: %s'% prefix)
-        objBa=ReadBA(args.init, objTemp.nTsai[1], prefix)
-        
-        #---------------------------------------------------------------
-        # Read next steps
-        #---------------------------------------------------------------
-        lstBaName=[]
 
-        for pathBA in args.ba:
-            logger.info('# Read %s'% os.path.basename(pathBA))
-            lstBaName.append(os.path.basename(pathBA))
-            objBa.AddBA(pathBA)
-        
-        if objBa.Valid(): 
-            print(objBa)
-            raise RuntimeError(objBa.Valid())
-        
+        #---------------------------------------------------------------
+        # Read BA
+        #---------------------------------------------------------------
+        if args.table or args.longTable or args.graphEO:
+            logger.info('# Read BA')
+            logger.info('Prefix: %s'% prefix)
+
+            objBa=ReadBA(args.init, objTemp.nTsai[1], prefix)
+            
+            lstBaName=[]
+            for pathBA in args.ba:
+                logger.info(os.path.basename(pathBA))
+                lstBaName.append(os.path.basename(pathBA))
+                objBa.AddBA(pathBA)
+            
+            if objBa.Valid(): 
+                print(objBa)
+                raise RuntimeError(objBa.Valid())
+        #---------------------------------------------------------------
+        # Read KP
+        #---------------------------------------------------------------
+        if args.graphKP:
+            logger.info('# Read KP')
+
+            lstKP=[]
+            lstBaName=[]
+            for pathBA in args.ba:
+                logger.info(os.path.basename(pathBA))
+                lstBaName.append(os.path.basename(pathBA))
+                lstKP.append(KP2Matrix(pathBA))
+
         #---------------------------------------------------------------
         # Tables
         #---------------------------------------------------------------
@@ -374,21 +443,32 @@ if __name__ == "__main__":
                 logger.info('# Longue Table:')
             else:
                 logger.info('# Summary Table:')
-            Table(objBa.dicTsai, len(args.ba), longT=args.longTable, intrinsic=args.io)
+            Table(objBa.dicTsai, len(args.ba), args)
         
         #---------------------------------------------------------------
         # Graph
         #---------------------------------------------------------------
-        if args.graph:
+        if args.graphEO:
             logger.info('# Camera Position Graph')
-            Graph(objBa.dicTsai, args)
+            GraphEO(objBa.dicTsai, args)
 
         #---------------------------------------------------------------
         # Key Point Sketch
         #---------------------------------------------------------------
-        if args.kp:
-            logger.info('# Key Point Sketch')
-            SketchKP(objBa.dicTsai, args)
+        if args.graphKP:
+            logger.info('# Key Point Displacement')
+            print('='*(colLen[0]+10)+'{:^26}'.format('Statistics')+'='*(13+colLen[1]*(len(lstBaName)-1)+3*(len(lstBaName)-2)))
+            Table_Line('ImgID', 'Key', lstBaName)
+            
+            matKP=np.array(lstKP)
+            
+            print('Initial Residuals:')
+            Table_Sum(matKP[:,:,3].T, 'Init res')
+            print('Final Residuals:')
+            Table_Sum(matKP[:,:,7].T, 'Final res')
+            
+            logger.info('# Key Point Graph')
+            GraphKP(lstKP, args)
         
         #---------------------------------------------------------------
         # Matplotlib Stdout
